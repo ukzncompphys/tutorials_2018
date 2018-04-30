79fd8efe971b08af37a592229915ef045402d06e
Jonathan Sievers <sievers@cita.utoronto.ca>
2018-04-30 02:32:04 +0200

    added simpson's rule answer code

---
+++ simpsons.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def simpsons(fun,x0,x1,n):
+    #first, let's check we got a valid input for n.
+    if not(type(n)==int):  
+        print 'number of points must be an integer'
+        return None
+    if n&1==0:
+        print 'number of points must be odd in simpsons'
+        return None
+    x=np.linspace(x0,x1,n)
+    dx=x[1]-x[0]
+    y=fun(x)  #this calls the function we passed in at the x points
+    even_sum=y[2:-1:2].sum() #make sure we don't pick up last point
+    odd_sum=y[1::2].sum()
+    tot=y[0]+y[-1]+4*odd_sum+2*even_sum
+    return dx*tot/3
+
+n=[11,31,101,301,1001]
+
+nvec=np.asarray(n)
+errvec=np.zeros(len(n))
+for i in range(len(n)):
+    err=simpsons(np.cos,0,np.pi/2,n[i])-1
+    print 'Simpsons error on ',n[i],' points is ',err
+    errvec[i]=np.abs(err)
+
+plt.ion()
+plt.clf();
+plt.plot(nvec,errvec)
+ax=plt.gca()
+ax.loglog() #here's another way to specify log axes
+
+pp=np.polyfit(np.log(nvec),np.log(errvec),1)
+print 'Error goes like number of point to the power ',pp[0]
+pred=np.polyval(pp,np.log(nvec))
+plt.plot(nvec,np.exp(pred))
+plt.legend(['Simpsons Error','Power Law Fit'])
